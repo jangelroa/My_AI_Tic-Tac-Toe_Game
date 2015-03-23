843e304ed7ec675f70b2667efc59fbf5a21d303e
jangelroa <jangelroa@gmail.com>
2015-03-23 11:58:51 -0700

    Documented all methods so far

--- app.js
+++ app.js
@@ -1,48 +1,111 @@
+// Program:     Unbeatable Tic-Tac-Toe Game
+// Description:
+//              Build a Tic-Tac-Toe game that never can be defeated.
+
+// Author:      Angel Roa
+// Date:        March 23, 2015
 
 var show_console = true;
 
-var is_X_winner  = function( state ) {
+//*******************************************************************
+//         cl( arg1, arg2, arg3 )
+//*******************************************************************
+// Task: Turn on and off all the console.logs.
+// @params arg1, arg2, arg3 -  params for console.log
+// @return No need it.
+//
+function cl( arg1, arg2, arg3 ) {
+  if( show_console ) {
+
+    if( arg3 ) {
+      console.log( arg1, arg2, arg3 );
+    } else if( arg2 ) {
+      console.log( arg1, arg2 );
+    }else {
+      console.log( arg1 );
+    }
+  }
+} // end of cl( arg1, arg2, arg3 ) {}
+
+//*******************************************************************
+//                  is_X_winner( state )
+//*******************************************************************
+// Task: Call winner_is with "state" and "X" as params.
+// @param: state -  array of the sate of the game.
+// @return: The result of calling winner_is( state, "X").
+//
+function is_X_winner( state ) {
   return winner_is( state, "X");
-}
-var is_O_winner  = function( state ) {
+} // end of is_X_winner( state ) {}
+
+
+//*******************************************************************
+//                  is_O_winner( state )
+//*******************************************************************
+// Task: Call winner_is with "state" and "O" as params.
+// @param: state -  array of the sate of the game.
+// @return: The result of calling winner_is( state, "O").
+//
+function is_O_winner( state ) {
   return winner_is( state, "O");
-}
+} // end of is_O_winner( state )
 
 
-var winner_is = function( state, player ) {
+//*******************************************************************
+//                winner_is( state, player )
+//*******************************************************************
+//  Task: Check if player won the game.
+//  @param: state -  array of the sate of the game.
+//  @param: player -  "X" or "O".
+//  @return: True if player won the game or false otherwise.
+//
+function winner_is( state, player ) {
 
 //checking horizontal lines
-  if( show_console ) { console.log(""); }
-  if( show_console ) {  console.log("checking horizontal lines:"); }
+  cl("");
+  cl("checking horizontal lines for player = \"" + player + "\":");
   for( var i = 0 ; i <= 6 ; i += 3 ) {
-      if( show_console ) {  console.log(""); }
-      if( show_console ) {  console.log("row ",i); }
-      // check_horizontal_line(i, player);
+      cl( "" );
+      cl( "row starting with index " + i );
 
-      if( check_horizontal_line(i, player) ) {
+      if( check_horizontal_lines( i, player ) ) {
+        cl("row starting with index " + i + " is a WINNER line" );
         return true;
       }
   }
 
 //checking vertical lines
-  if( show_console ) {  console.log(""); }
-  if( show_console ) {  console.log("checking vertical lines:"); }
+  cl("");
+  cl("checking vertical lines for player = \"" + player + "\":");
   for( var i = 0 ; i <= 2 ; i ++ ) {
-      if( show_console ) {  console.log(""); }
-      if( show_console ) {  console.log("column ",i); }
-      // check_horizontal_line(i, player);
+      cl( "" );
+      cl( "column starting with index " + i );
 
-      if( check_vertical_line(i, player) ) {
+      if( check_vertical_lines( i, player ) ) {
+        cl("column starting with index " + i + " is a WINNER line" );
         return true;
       }
   }
 
+//checking diagonal lines
+  cl("");
+  cl("checking diagonal lines for player = \"" + player + "\":");
+
+
+
   return false;
-}
+} // end of winner_is( state, player )
 
-
-
-var check_horizontal_line = function(index, player) {
+//*******************************************************************
+//         check_horizontal_line( index, player )
+//*******************************************************************
+// Task: Check if the horizontal line starting with "index" is a
+//       winner line for "player".
+// @param: index -  index of the state array.
+// @param: player -  "X" or "O".
+// @return: True if this is a winner line or false otherwise.
+//
+function check_horizontal_lines( index, player ) {
 
   var is_this_a_winner_line = true;
 
@@ -53,9 +116,18 @@ var check_horizontal_line = function(index, player) {
       }
   }
   return is_this_a_winner_line;
-}
+} // end of check_horizontal_line( index, player )
 
-var check_vertical_line = function(index, player) {
+//*******************************************************************
+//         check_vertical_lines( index, player )
+//*******************************************************************
+// Task: Check if the vertical line starting with "index" is a
+//       winner line for "player".
+// @param: index -  index of the state array.
+// @param: player -  "X" or "O".
+// @return: True if this is a winner line or false otherwise.
+//
+function check_vertical_lines( index, player ) {
 
   var is_this_a_winner_line = true;
 
@@ -66,15 +138,14 @@ var check_vertical_line = function(index, player) {
       }
   }
   return is_this_a_winner_line;
-}
+} // end of check_vertical_lines( index, player )
 
 
 
 
 var state = ["X","O","_","X","O","X","_","O","O"];
-// if( show_console ) {  console.log(winner_is(state, "O")); }
-if( show_console ) {  console.log(is_X_winner( state )); }
-if( show_console ) {  console.log(is_O_winner( state )); }
+cl(is_X_winner( state ));
+cl(is_O_winner( state ));
 
 
 
